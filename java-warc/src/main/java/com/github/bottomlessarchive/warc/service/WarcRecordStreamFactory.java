15845ea55558013fe07a648d7e0d59614721ed73
Gyula Lakatos <laxika91@gmail.com>
2020-01-16 15:35:38 +0100

    #3 - Added a new parameter to the WarcRecordStreamFactory methods.

--- java-warc/src/main/java/com/github/bottomlessarchive/warc/service/WarcRecordStreamFactory.java
+++ java-warc/src/main/java/com/github/bottomlessarchive/warc/service/WarcRecordStreamFactory.java
@@ -1,42 +1,69 @@
 package com.github.bottomlessarchive.warc.service;
 
 import com.github.bottomlessarchive.warc.service.record.domain.WarcRecord;
+import com.github.bottomlessarchive.warc.service.record.domain.WarcRecordType;
+import lombok.NonNull;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.Spliterator;
-import java.util.Spliterators;
+import java.util.*;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 @SuppressWarnings("unused")
 public class WarcRecordStreamFactory {
 
-    public static Stream<WarcRecord> streamOf(final URL url) {
+    private static final List<WarcRecordType> EVERY_WARC_RECORD_TYPE = Arrays.asList(WarcRecordType.values());
+
+    public static Stream<WarcRecord> streamOf(@NotNull @NonNull final URL url) {
+        return streamOf(url, EVERY_WARC_RECORD_TYPE);
+    }
+
+    public static Stream<WarcRecord> streamOf(@NotNull @NonNull final URL url,
+            @NotNull @NonNull final List<WarcRecordType> requiredRecordTypes) {
         try {
             return streamOf(new AvailableInputStream(new BufferedInputStream(url.openStream())),
-                WarcReader.DEFAULT_CHARSET, true);
+                    WarcReader.DEFAULT_CHARSET, true, requiredRecordTypes);
         } catch (IOException e) {
             throw new WarcNetworkException("Unable to open WARC location: " + url + "!", e);
         }
     }
 
-    public static Stream<WarcRecord> streamOf(final InputStream warcFileLocation) {
-        return streamOf(warcFileLocation, WarcReader.DEFAULT_CHARSET);
+    public static Stream<WarcRecord> streamOf(@NotNull @NonNull final InputStream warcFileLocation) {
+        return streamOf(warcFileLocation, EVERY_WARC_RECORD_TYPE);
     }
 
-    public static Stream<WarcRecord> streamOf(final InputStream warcFileLocation, final Charset charset) {
-        return streamOf(new BufferedInputStream(warcFileLocation), charset, true);
+    public static Stream<WarcRecord> streamOf(@NotNull @NonNull final InputStream warcFileLocation,
+            @NotNull @NonNull final List<WarcRecordType> requiredRecordTypes) {
+        return streamOf(warcFileLocation, WarcReader.DEFAULT_CHARSET, requiredRecordTypes);
     }
 
-    public static Stream<WarcRecord> streamOf(final InputStream inputStream, final Charset charset,
-        final boolean compressed) {
+    public static Stream<WarcRecord> streamOf(@NotNull @NonNull final InputStream warcFileLocation,
+            @NotNull @NonNull final Charset charset) {
+        return streamOf(new BufferedInputStream(warcFileLocation), charset, true, EVERY_WARC_RECORD_TYPE);
+    }
+
+    public static Stream<WarcRecord> streamOf(@NotNull @NonNull final InputStream warcFileLocation,
+            @NotNull @NonNull final Charset charset, @NotNull @NonNull final List<WarcRecordType> requiredRecordTypes) {
+        return streamOf(new BufferedInputStream(warcFileLocation), charset, true, requiredRecordTypes);
+    }
+
+    public static Stream<WarcRecord> streamOf(@NotNull @NonNull final InputStream inputStream,
+            @NotNull @NonNull final Charset charset, final boolean compressed) {
+        return streamOf(inputStream, charset, compressed, EVERY_WARC_RECORD_TYPE);
+    }
+
+    public static Stream<WarcRecord> streamOf(@NotNull @NonNull final InputStream inputStream,
+            @NotNull @NonNull final Charset charset, final boolean compressed,
+            @NotNull @NonNull final List<WarcRecordType> requiredRecordTypes) {
         final WarcReader warcReader = new WarcReader(inputStream, charset, compressed);
 
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(
-            new SafeWarcRecordIterator(warcReader), Spliterator.ORDERED | Spliterator.NONNULL), false);
+                new SafeWarcRecordIterator(warcReader), Spliterator.ORDERED | Spliterator.NONNULL), false)
+                .filter(warcRecord -> requiredRecordTypes.contains(warcRecord.getType()));
     }
 }
